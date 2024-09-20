b04814dafcd97213227483eabbed0117ff629652
Maxsuell F <89057455+MxaYa@users.noreply.github.com>
2024-09-20 00:15:12 -0300

    Omaga

--- Server/index.js
+++ Server/index.js
@@ -1,7 +1,25 @@
-const app = require ('express')()
-const server = require('http').createServer(app);
-const io = require('socket.io')(server, {cors: {origin: 'http://localhost:5173'}})
+const express = require("express");
+const cors = require("cors");
+const axios = require ("axios");
 
-const PORT = 3001
+const app = express();
+app.use(express.json());
+app.use(cors({ origin: true }));
 
-server.listen(PORT, () => console.log('Server running...'))
+app.post("/authenticate", async (req, res) => {
+  const { username } = req.body;
+
+  try{
+    const r = await axios.put(
+        'https://api.chatengine.io/users/',
+        {username: username, secret: username, first_name: username},
+        { headers: { 'Private-Key': '9a3d83fd-f5a6-4b1f-be82-9b77a306639a' } }
+    )
+    return res.status(r.status).json(r.data);
+  } catch(e) {
+    return res.status(e.response.status).json(e.response.data);
+  }
+  return res.json({ username: username, secret: "sha256..." });
+});
+
+app.listen(3001);
